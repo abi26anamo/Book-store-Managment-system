afcb4d8f12115b61e2213f63a3a9b9fd2baafa64
abi26anamo <abianamo282@gmail.com>
2021-03-23 16:44:30 +0300

    update some files

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
+import userRoute from "./Routes/UserRoutes.js";
+import dotenv from "dotenv";
+import connectDatabase from "./config/MongoDb.js";
+import orderRouter from "./Routes/orderRoutes.js";
+import productRoute from "./routes/BookRoutes.js";
+
+dotenv.config()
+const app =express();
+connectDatabase();
+app.use(express.json())
+app.use("/api/users",userRoute)
+app.use("/api/orders",orderRouter)
+app.use("/api/books", productRoute);
+app.use(notFound)
+app.use(errorHandler)
+
+app.get("/",(req,res)=>{
+    res.send("API is Running...");
+})
+
+const PORT=process.env.PORT ||1000;
+app.listen(PORT,console.log(`server run in port ${PORT}`));
